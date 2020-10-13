ccf39613bab229eb4eacfcf934123c24db690757
Ashish Rao <ashishrao2598@gmail.com>
2020-10-13 15:58:29 +0530

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  # Returns the gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = generate_gravatar_id(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
+  def generate_gravatar_id(email)
+    Digest::MD5::hexdigest(email)
+  end
 end
